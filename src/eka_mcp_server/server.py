b942cde86760fab87cdfa6d28575ac5f48f00851
Nikhil Kasukurthi <nikhil.kasukurthi@eka.care>
2025-07-24 16:56:51 +0530

    Add dxt manifest.json

--- src/eka_mcp_server/server.py
+++ src/eka_mcp_server/server.py
@@ -19,20 +19,20 @@ async def main() -> None:
     # Configure logging
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.StreamHandler()
-        ]
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler()],
     )
 
     logger = logging.getLogger("main")
     logger.info("Starting Eka MCP server..")
 
     logger.info("Validating server arguments..")
-    parser = argparse.ArgumentParser(description='Eka MCP server. Documentation available at - https://github.com/eka-care/eka_mcp_server/blob/main/README.md')
-    parser.add_argument('--eka-api-host', required=True, help='EKA MCP API Host')
-    parser.add_argument('--client-id', required=False, help='EKA MCP API Client ID')
-    parser.add_argument('--client-secret', required=False, help='EKA MCP Client Secret')
+    parser = argparse.ArgumentParser(
+        description="Eka MCP server. Documentation available at - https://github.com/eka-care/eka_mcp_server/blob/main/README.md"
+    )
+    parser.add_argument("--eka-api-host", required=True, help="EKA MCP API Host")
+    parser.add_argument("--client-id", required=False, help="EKA MCP API Client ID")
+    parser.add_argument("--client-secret", required=False, help="EKA MCP Client Secret")
 
     args = parser.parse_args()
     # Initialize the EkaMCP client
@@ -40,7 +40,7 @@ async def main() -> None:
         eka_api_host=args.eka_api_host,
         client_id=args.client_id,
         client_secret=args.client_secret,
-        logger=logger
+        logger=logger,
     )
 
     # Initialize and run the MCP server
@@ -53,7 +53,7 @@ async def main() -> None:
             write_stream,
             InitializationOptions(
                 server_name="eka_mcp_server",
-                server_version="0.1.0",
+                server_version="0.3.5",
                 capabilities=server.get_capabilities(
                     notification_options=NotificationOptions(),
                     experimental_capabilities={},
